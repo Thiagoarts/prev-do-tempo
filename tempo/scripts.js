abd9a574aad443334ad8c85f91487a4613692731
Thiago da Silva Benedito <115752071+Thiagoarts@users.noreply.github.com>
2024-05-19 11:28:47 -0300

    Add files via upload

---
+++ tempo/scripts.js
@@ -0,0 +1,22 @@
+
+
+const key = "19c70054e44c6f78938b0969a5737737"
+
+function colocarDadosNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C";
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description;
+    document.querySelector(".umidade").innerHTML = "Umidade " + dados.main.humidity + "%";
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+}
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(Response => Response.json());
+    colocarDadosNaTela(dados);
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value;
+    buscarCidade(cidade);
+}
